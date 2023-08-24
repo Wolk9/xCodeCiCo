735548378189d648e1c4474904a265bb34dd4921
Martin op iMac <wolk9@me.com>
2023-08-24 18:48:46 +0200

    Initial Commit

---
+++ CICO/CICOApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICOApp.swift
+//  CICO
+//
+//  Created by Macbook  on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CICOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
